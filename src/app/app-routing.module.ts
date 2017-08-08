a30094b2ea19f01ecc9bf01ff643d04bfebc9c5b
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-08-08 15:33:27 +0000

    Controlling navigation with CanDeactive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import {ServerComponent} from './servers/server/server.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {AuthGuard} from './auth-guard.service';
+import {CanDeactivateGuard} from './servers/edit-server/can-deactivate-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
 /*  { path: 'servers', canActivate: [AuthGuard] , component: ServersComponent, children: [*/
   { path: 'servers', canActivateChild: [AuthGuard] , component: ServersComponent, children: [
     { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent }
+    { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
   ] },
   { path: 'not-found', component: PageNotFoundComponent },
   // the order of '**' is very important, make sure this is the last route
