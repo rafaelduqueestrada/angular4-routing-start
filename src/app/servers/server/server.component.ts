1c97ea5158108d497cd2bf33cf8cb0d184b76ea0
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-08-08 16:21:19 +0000

    Understanding location strategies

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ActivatedRoute, Data, Params, Router} from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -16,15 +16,21 @@ export class ServerComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
+      }
+    );
+
     // + -> convert to a number
-    const id = +this.route.snapshot.params['id'];
+/*    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe(
       (params: Params) => {
         // + -> convert to a number
         this.server = this.serversService.getServer(+params['id']);
       }
-    );
+    );*/
   }
 
 
